eb6f8f1ff088b05a6a442a094d60505a4988a028
Guillermo Moguel Mandujano <gmoguel@unich.edu.mx>
2014-10-29 17:23:43 -0600

    gh-pages

---
+++ README.md
@@ -0,0 +1,25 @@
+# Clima de Ciudades
+* * *
+Descripción: Consumo de datos con backbone.js
+
+
+### Librerias
+* [http://jquery.com/](http://jquery.com/)
+* [http://getbootstrap.com/](http://getbootstrap.com/)
+* [http://backbonejs.org/](http://backbonejs.org/)
+* [http://underscorejs.org/](http://underscorejs.org/)
+* [http://epeli.github.io/underscore.string/](http://epeli.github.io/underscore.string/)
+
+### Recursos
+* [http://openweathermap.org](http://openweathermap.org)
+* [http://restcountries.eu](http://restcountries.eu)
+* [http://www.geonames.org](http://www.geonames.org)
+
+#### Referencias
+* [Listados, Filtors, Ajax, json, en AngularJS](https://www.youtube.com/watch?v=diaCDrTQN5M&list=PLIcuwIrm4rKdPA5DOzbjSKjJXECfzumZf&index=11)
+* [http://stackoverflow.com/questions/18112627/filter-collection-by-substring-in-backbone-js](http://stackoverflow.com/questions/18112627/filter-collection-by-substring-in-backbone-js)
+* [http://stackoverflow.com/questions/13727074/backbone-js-filter-on-any-value-in-a-collection](http://stackoverflow.com/questions/13727074/backbone-js-filter-on-any-value-in-a-collection)
+* [Multi-Sort A Backbone Collection](https://coderwall.com/p/s6r_ga)
+
+
+*Guillermo Moguel Mandujano [G+](https://www.google.com/+GuillermoMoguel) [tw](https://twitter.com/chipocrudos)
---
+++ js/main.js
@@ -0,0 +1,191 @@
+(function () {
+
+/* models */
+	//modelo paises
+	var CountriesModel = Backbone.Model.extend();
+
+	//modelo weather
+	var WeatherModel = Backbone.Model.extend({
+		initialize: function(option){
+			this.capital = option.capital;
+			this.alpha2Code = option.alpha2Code;
+		},
+		url: function(){
+			return 'http://api.openweathermap.org/data/2.5/weather?q=' + encodeURIComponent(this.capital) + ','+ encodeURIComponent(this.alpha2Code) + '&units=metric';
+		},
+	});
+
+/* collections */
+	//Collecion paises
+	var CountriesCollection = Backbone.Collection.extend({
+		model: CountriesModel,
+		initialize: function(option){
+			this.region = option.region;
+			this.fetch();
+			
+		},
+		url: function() {
+			return 'http://restcountries.eu/rest/v1/region/' + encodeURIComponent(this.region);
+		},
+		customSearch: function(filters){
+			return this.filter(function(model) {
+				var add = false;
+				return _.any(model.values(), function(value) {
+					if(!_.isObject(value)){
+				        if (_.isNumber(value)){
+				            value = value.toString();
+				        }
+				        if (_.isString(value)){
+				            valuel = value.toLowerCase();
+			            	if ((value.search(filters) !=-1 || valuel.search(filters) !=-1) && !add){
+			            		add = true;
+			            		return ~value.indexOf(filters)!== -1;
+			            	}
+				        }
+			        }
+				});
+			});
+  		}
+	});
+
+	var ResultsCountries = Backbone.Collection.extend({
+		model: CountriesModel,
+        /**
+	    * Sort by supplied attributes.  First param is sorted first, and
+	    * last param is final subsort
+	    * @param {String} sortAttributes
+	    * @example collection.sortBy("last_name","first_name")
+	    */
+	    sortBy : function(sortAttributes){
+	        var attributes = arguments;
+                if(attributes.length){
+                	this.models = this._sortBy(this.models,attributes);
+            	}   
+	    },
+	    /**
+	     * Recursive sort
+	     */
+	    _sortBy : function(models,attributes){
+	        var attr,
+	                that = this;
+	        //base case
+	        if(attributes.length === 1){
+	            attr = attributes[0];
+	            return _(models).sortBy(function(model){
+	                return model.get(attr);
+	            });
+	        }
+	        else{
+	            attr = attributes[0];
+	            attributes = _.last(attributes,attributes.length-1);
+
+	            //split up models by sort attribute, 
+	            //then call _sortBy with remaining attributes
+	            models = _(models).chain().
+	                sortBy(function(model){
+	                    return model.get(attr);
+	                }).
+	                groupBy(function(model){
+	                    return model.get(attr);
+	                }).
+	                toArray().
+	                value();
+
+	            _(models).each(function(modelSet,index){
+	                models[index] = that._sortBy(models[index],attributes);
+	            });
+	            return _(models).flatten(); 
+	        }
+	    }
+	});
+
+/* views */
+	//Region list render view
+	var RegionView = Backbone.View.extend({
+		el:'#region',
+		events:{
+			'change': 'changeValue',
+		},
+		changeValue: function(){
+			var region = $(this.el).val();
+			if (region){
+				var countriescollection = new CountriesCollection({region:region});
+				var countrieslistview = new CountriesListView({collection:countriescollection});
+			}
+		},
+	});
+
+	//Region list render view
+	var CountriesListView = Backbone.View.extend({
+		el:'aside',
+		template: _.template($('#selectOption').html()),
+		initialize: function(){
+			this.render();
+			this.results = new ResultsCountries();
+			this.listenTo( this.collection, 'add', this.renderCountries);
+		},
+		events:{
+			'keyup #countries': 'searchValue',
+			'click .btn-order': 'orderResult',
+		},
+		render: function (){
+			$('section').html('');
+			this.order = this.$('.btn-order.btn-primary').attr('id');
+		},
+		renderList: function(){
+			$('section').html('');
+			this.results.sortBy(this.order);
+			this.results.each(function(item){
+			 	var countryview = new CountryView({model:item});
+			});
+		},
+		renderCountries: function( item ){
+			var weather = new WeatherModel({
+				capital: item.get('capital'),
+				alpha2Code: item.get('alpha2Code')
+			});
+			weather.fetch({
+				success:function(){
+					item.set({weather:weather.toJSON()});
+					var countryview = new CountryView({model:item});
+				},
+				error: function(){
+					item.set({weather:false});
+					var countryview = new CountryView({model:item});
+				}
+
+			});
+		},
+		searchValue: function(event){
+			event.preventDefault();
+			var letters = this.$('#countries').val();
+			if (letters.length > 1){
+				this.results.reset(this.collection.customSearch(letters));
+			}else{
+				this.results.reset(this.collection.toArray());
+			}
+			this.renderList();
+		},
+		orderResult: function(event){
+  			this.$('.btn-order').removeClass('btn-primary');
+  			$(event.target).addClass('btn-primary');
+  			this.order = $(event.target).attr('id');
+  			if (!this.results.length) this.results.reset(this.collection.toArray());
+  			this.renderList();
+  		},
+	});
+
+	var CountryView = Backbone.View.extend({
+		el:'section',
+		template: _.template($('#countrydata').html()),
+		initialize: function(){
+			this.render();
+		},
+		render: function(){
+			this.$el.append( this.template( this.model.toJSON() ));
+		}
+	});
+/* asignación de objectos*/
+	//region
+	var regionlistview = new RegionView();
+}());
